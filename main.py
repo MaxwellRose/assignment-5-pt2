0146ee2b0a5570cf4156b15e70514adad7db76f6
Maxwell Rose <maxwell.rose@ocsbstudent.ca>
2021-03-31 11:36:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+diameter = int(input("Enter diameter to get circumference:"))
+pi = 3.14
+circumference  = 0
+circumference  = 2 * pi * (diameter / 2)
+print(circumference)
+
+diameter1 = int(input("Enter diameter to get area:"))
+area  = 0
+area  = pi * (diameter1 / 2) * (diameter1 / 2)
+print(area)
